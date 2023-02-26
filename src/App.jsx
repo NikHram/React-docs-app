ff0aa4859aa8f76d82241d21036763e1d76be9c1
NikHram <47941985+NikHram@users.noreply.github.com>
2023-02-27 01:23:05 +0200

    logic & styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,57 @@
-import Square from "./Square";
+import { useState } from "react";
+import Board from "./Board";
 
 const App = () => {
-  return <Square></Square>;
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+  let moves = convertToListItems(history);
+
+  function convertToListItems(element) {
+    return element.map((_, move) => {
+      if (move === currentMove) {
+        return (
+          <li key={move}>
+            {move === 0 ? `Starting position` : `You are at the move #${move}`}
+          </li>
+        );
+      }
+      let description = move > 0 ? `Go to move # ${move}` : "Go to game start";
+      return (
+        <li key={move}>
+          <button
+            onClick={() => {
+              const elements = document.getElementsByClassName("square");
+              for (let i = 0; i < elements.length; i++) {
+                elements[i].classList.remove("highlight");
+              }
+              setCurrentMove(move);
+            }}
+          >
+            {description}
+          </button>
+        </li>
+      );
+    });
+  }
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol id="list">{moves}</ol>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- src/Square.jsx
+++ src/Square.jsx
@@ -1,3 +1,9 @@
-export default function Square() {
-  return <button className="square">X</button>;
-}
+const Square = ({ value, onSquareClick }) => {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+};
+
+export default Square;
